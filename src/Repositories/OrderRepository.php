b01da666f32a1badc708f54e627f1781d1001f4a
Matheus Lago Frete <mlfrete@gmail.com>
2023-03-16 13:46:56 -0300

    release-0.1

--- src/Repositories/OrderRepository.php
+++ src/Repositories/OrderRepository.php
@@ -5,9 +5,9 @@ namespace Helte\HermesSdk\Repositories;
 use Helte\HermesSdk\Schema\Builder;
 use Helte\HermesSdk\Schema\SchemaRepository;
 
-class BudgetRepository extends SchemaRepository
+class OrderRepository extends SchemaRepository
 {
-    protected static $base_uri = 'budgets';
+    protected static $base_uri = 'orders';
 
     public static function from($uri) : Builder{
         return self::query();
