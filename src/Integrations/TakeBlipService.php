46e232af75569745c0498e7441bf4f9e6b85fda0
Matheus Lago Frete <mlfrete@gmail.com>
2023-03-20 09:39:50 -0300

    send whatsapp message

---
+++ src/Integrations/TakeBlipService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Helte\HermesSdk\Integrations;
+
+use Helte\HermesSdk\Services\HermesService;
+
+class TakeBlipService
+{
+    public static function sendWhatsAppMessage(int $client_id, array $message_params, $phone, $template, $force = false){
+        HermesService::dispatchJob(
+            'SendTakeBlipMessage',
+            [
+                'namespace' => 'App\Integrations\TakeBlip\Jobs',
+                'private_params' => ['client_id' => $client_id, 'message_params' => $message_params, 'phone', $phone, 'template' => $template, 'force' => $force]
+            ]
+        );
+    }
+}
