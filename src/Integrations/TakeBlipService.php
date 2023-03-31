dd8cf46da82d8b71b7646b6fc0431cfbffe73872
Matheus Lago Frete <mlfrete@gmail.com>
2023-03-31 09:47:58 -0300

    Update TakeBlipService.php

--- src/Integrations/TakeBlipService.php
+++ src/Integrations/TakeBlipService.php
@@ -15,4 +15,25 @@ class TakeBlipService
             ]
         );
     }
+
+    public static function sendWhatsAppMessageWithPostState(int $client_id, array $message_params, $phone, $template, $state, $flow, $force = false){
+        HermesService::dispatchJob(
+            'SendTakeBlipMessage',
+            [
+                'namespace' => 'App\Integrations\TakeBlip\Jobs',
+                'private_params' => 
+                    ['client_id' => $client_id, 
+                    'message_params' => $message_params, 
+                    'phone' => $phone, 
+                    'template' => $template, 
+                    'force' => $force, 
+                    'transition' => [
+                        'moment' => 'post',
+                        'state' => $state,
+                        'flow' => $flow
+                    ]
+                ]
+            ]
+        );
+    }
 }
